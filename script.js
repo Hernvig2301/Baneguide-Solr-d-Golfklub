d72623b640f31f270b087bd7926463c4fc8bfad1
Hernvig2301 <rasmushernvig@gmail.com>
2024-05-29 11:08:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+   $(".hide").hide();
+    
+   $(".marker-one").click(function(){
+      if ($(".one-txt").is(":visible")) {
+          $(".one-txt").fadeOut(300);
+      } else {
+          $(".hide").hide();
+          $(".one-txt").fadeIn(300);
+      }
+  });
+    
+  $(".marker-two").click(function(){
+   if ($(".two-txt").is(":visible")) {
+       $(".two-txt").fadeOut(300);
+   } else {
+       $(".hide").hide();
+       $(".two-txt").fadeIn(300);
+   }
+});
+    
+$(".marker-three").click(function(){
+   if ($(".three-txt").is(":visible")) {
+       $(".three-txt").fadeOut(300);
+   } else {
+       $(".hide").hide();
+       $(".three-txt").fadeIn(300);
+   }
+});
+    
+$(".marker-four").click(function(){
+   if ($(".four-txt").is(":visible")) {
+       $(".four-txt").fadeOut(300);
+   } else {
+       $(".hide").hide();
+       $(".four-txt").fadeIn(300);
+   }
+});
+
+$(".marker-five").click(function(){
+   if ($(".five-txt").is(":visible")) {
+       $(".five-txt").fadeOut(300);
+   } else {
+       $(".hide").hide();
+       $(".five-txt").fadeIn(300);
+   }
+});
+
+$(".marker-six").click(function(){
+   if ($(".six-txt").is(":visible")) {
+       $(".six-txt").fadeOut(300);
+   } else {
+       $(".hide").hide();
+       $(".six-txt").fadeIn(300);
+   }
+});
+
+$(".marker-seven").click(function(){
+   if ($(".seven-txt").is(":visible")) {
+       $(".seven-txt").fadeOut(300);
+   } else {
+       $(".hide").hide();
+       $(".seven-txt").fadeIn(300);
+   }
+});
+
+$(".marker-eight").click(function(){
+   if ($(".eight-txt").is(":visible")) {
+       $(".eight-txt").fadeOut(300);
+   } else {
+       $(".hide").hide();
+       $(".eight-txt").fadeIn(300);
+   }
+});
+
+$(".marker-nine").click(function(){
+   if ($(".nine-txt").is(":visible")) {
+       $(".nine-txt").fadeOut(300);
+   } else {
+       $(".hide").hide();
+       $(".nine-txt").fadeIn(300);
+   }
+});
+  
+  });
